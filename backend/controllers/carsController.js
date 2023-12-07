4e9510c2ac0e9423569bb2e73b1628514b6572a7
omar <yossef>
2023-12-07 09:32:34 +0100

    update cars

--- backend/controllers/carsController.js
+++ backend/controllers/carsController.js
@@ -1,15 +1,31 @@
-import { JSONPreset } from "lowdb/node";
-import "dotenv/config";
-const defualtCarsData = { cars: [] };
-const db = await JSONPreset("db.json", defualtCarsData);
-console.log(db.data);
-
+// import { JSONPreset } from "lowdb/node";
+// import "dotenv/config";
+// const defualtCarsData = { cars: [] };
+// const db = await JSONPreset("db.json", defualtCarsData);
+// console.log(db.data);
+const cars = [
+  {
+    id: 1,
+    name: "Ford",
+    year: 2020,
+  },
+  {
+    id: 2,
+    name: "BMV",
+    year: 2022,
+  },
+  {
+    id: 3,
+    name: "KIA",
+    year: 2021,
+  },
+];
 export const getAllCars = async (req, res) => {
   try {
-    if ((process.env.NODE_ENV = "development")) {
-      await db.read();
-      const allCars = db.data.cars;
-      const extandetCar = allCars.map((car) => {
+    if (process.env.NODE_ENV === "development") {
+      // await db.read();
+      // const allCars = db.data.cars;
+      const extandetCar = cars.map((car) => {
         return { ...car, added: new Date().toLocaleString("de") };
       });
       res.status(200).json(extandetCar);
