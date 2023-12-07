64809d63d3ec1d4ba9ceea180080e9a7c029627e
omar <yossef>
2023-12-07 11:40:38 +0100

    add lowdb packege

--- backend/controllers/carsController.js
+++ backend/controllers/carsController.js
@@ -1,8 +1,8 @@
-// import { JSONPreset } from "lowdb/node";
-// import "dotenv/config";
-// const defualtCarsData = { cars: [] };
-// const db = await JSONPreset("db.json", defualtCarsData);
-// console.log(db.data);
+import { JSONPreset } from "lowdb/node";
+import "dotenv/config";
+const defualtCarsData = { cars: [] };
+const db = await JSONPreset("db.json", defualtCarsData);
+console.log(db.data);
 const cars = [
   {
     id: 1,
@@ -23,14 +23,14 @@ const cars = [
 export const getAllCars = async (req, res) => {
   try {
     if (process.env.NODE_ENV === "development") {
-      // await db.read();
-      // const allCars = db.data.cars;
-      const extandetCar = cars.map((car) => {
+      await db.read();
+      const allCars = db.data.cars;
+      const extandetCar = allCars.map((car) => {
         return { ...car, added: new Date().toLocaleString("de") };
       });
       res.status(200).json(extandetCar);
     } else {
-      res.status(200).send(cars);
+      res.status(200).send(db.data.cars);
     }
   } catch (error) {
     res.status(500).send(error);
